303b3cd9e441c174fd98942c761682e9975a417c
Bogdan Falk <bogdan37falk@gmail.com>
2019-11-30 15:33:37 +0200

    Deploy for Heroku

--- Deploy.bat
+++ Deploy.bat
@@ -4,4 +4,5 @@ echo %CD%
 git add .
 git commit -m "Deploy for Heroku"
 echo %CD%
-git subtree push --prefix 01_Source/02/Web heroku master
+
+git subtree push --prefix /01_Source/02/Web heroku master
