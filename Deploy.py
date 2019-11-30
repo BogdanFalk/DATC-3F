d862809447cb49949312ed0705d9b01266251288
Bogdan Falk <bogdan37falk@gmail.com>
2019-11-30 16:03:04 +0200

    Deploy to Heroku

--- Deploy.py
+++ Deploy.py
@@ -1,1 +1,5 @@
-print "meow"
+import os
+print("Deploy to Heroku!")
+os.system("git add .")
+os.system("git commit -m \"Deploy to Heroku\"")
+os.system("git subtree push --prefix 01_Source/02_Web heroku master")
